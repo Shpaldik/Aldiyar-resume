dd7aece4d138fd7d037dde1c28e9801b879c8585
Aldyar <shpappy7@gmail.com>
2024-06-17 12:01:35 +0500

    Доработал мелочи 10.0

--- src/components/History.vue
+++ src/components/History.vue
@@ -1,55 +1,52 @@
 <template>
     <section ref="history" class="flex justify-center px-4">
-        <div class="text-center mx-auto">
-            <h1 class="text-3xl font-bold">История развития:</h1>
-            <div class="flex flex-col md:flex-row items-start mt-10">
-                <div class="block flex-1">
-                    <div class="mt-10 md:mt-20 mx-5 md:mx-20">
-                        <div class="flex gap-3 items-center justify-start md:justify-center">
-                            <img src="/public/csharp.svg" alt="" width="30">
-                            <h3 class="text-3xl font-bold">2021</h3>
-                        </div>
-                        <p class="mt-3 text-left md:text-center">
-                            Погрузился в программирование с Python, проходя курсы несколько месяцев. Обратил внимание на преимущества С# в разработке ПО и переключился на его изучение. Начал принимать заказы на фрилансе, создавая парсеры и автореги. Использовал современные технологии, включая .NET Framework/Core, а также HtmlAgilityPack, Selenium и Puppeteer.
-                        </p>
-                    </div>
-                    <div class="mt-20 md:mt-40 mx-5 md:mx-20">
-                        <div class="flex gap-3 items-center justify-start md:justify-center">
-                            <img src="/public/Icons/nuxt.svg" alt="" width="30">
-                            <h3 class="text-3xl font-bold">2023</h3>
-                        </div>
-                        <p class="mt-3 text-left md:text-center">
-                            Решил поступать в Американские вузы, поэтому начал активнее набивать своё порфтолио программиста. Для этого начал учить английский. Обновлял UI сайта BankaiShop и брал для себя сложные задания, адаптировал вёртску под праздники. Добавил такие функции как: отзывы и рейтинги, система рекомендации, интеграция с соц.сетями, подписка на уведомления о наличии товара и т.д
-                        </p>
-                    </div>
-                </div>
-                <img class="hidden md:block mt-10 md:mt-5 mx-auto" src="/public/Line.svg" alt="" width="150">
-                <div class="block flex-1 mt-10 md:mt-0">
-                    <div class="mt-10 md:mt-60 mx-5 md:mx-20">
-                        <div class="flex gap-3 items-center justify-start md:justify-center">
-                            <img src="/public/asp.svg" alt="asp" width="30">
-                            <h3 class="text-3xl font-bold">2022</h3>
-                            <img src="/public/angular.svg" alt="angular" width="30">
-                        </div>
-                        <p class="mt-3 text-left md:text-center">
-                            Решил попробовать себя в вебе и начал с фулстека (ASP.NET + Angular), но мне больше понравился творческий подход, поэтому выбрал фронтенд (Vue). Позже нашёл стажировку в компании BankaiShop, где успешно прошёл ТЗ на адаптацию дизайна сайта под стиль лето. Далее со временем улучшил интерактивные карточки товаров, улучшил поиск и SEO сайта, добавил сохранение фильтров.
-                        </p>
-                    </div>
-                    <div class="mt-20 md:mt-40 mx-5 md:mx-20">
-                        <div class="flex gap-2 items-center justify-start md:justify-center">
-                            <img src="/public/WorldSkills.png" alt="" width="50">
-                            <h3 class="text-3xl font-bold">2024</h3>
-                        </div>
-                        <p class="mt-3 text-left md:text-center">
-                            Записался на участие в региональном WorldSkills по вебу на следующий год, поэтому дали полное свободное посещение от уроков. Выбрал стек Vue.js и Node.js.  И поднял уровень английского до B2. После закрытия BankaiShop начал искать работу в других более крупных компаниях и создавать свой стартап.
-                        </p>
-                    </div>
-                </div>
+      <div class="text-center mx-auto">
+        <h1 class="text-3xl font-bold">История развития:</h1>
+        <div class="flex flex-col md:flex-row items-start mt-10">
+          <div class="block flex-1">
+            <div class="mt-10 md:mt-20 mx-5 md:mx-20">
+              <div class="flex gap-3 items-center justify-start md:justify-center">
+                <img src="/public/csharp.svg" alt="" width="30">
+                <h3 class="text-3xl font-bold">2021</h3>
+              </div>
+              <p class="mt-3 text-left md:text-center">
+                Начал изучать программирование с Python, затем переключился на C#. Принимал заказы на фрилансе, создавая парсеры и автореги. Использовал .NET Framework/Core, HtmlAgilityPack, Selenium и Puppeteer.
+              </p>
             </div>
+            <div class="mt-20 md:mt-40 mx-5 md:mx-20">
+              <div class="flex gap-3 items-center justify-start md:justify-center">
+                <img src="/public/Icons/nuxt.svg" alt="" width="30">
+                <h3 class="text-3xl font-bold">2023</h3>
+              </div>
+              <p class="mt-3 text-left md:text-center">
+                Работал над UI сайта BankaiShop, адаптировал верстку под праздники. Добавил отзывы, рейтинги, систему рекомендаций, интеграцию с соцсетями и подписку на уведомления.
+              </p>
+            </div>
+          </div>
+          <img class="hidden md:block mt-10 md:mt-5 mx-auto" src="/public/Line.svg" alt="" width="150">
+          <div class="block flex-1 mt-10 md:mt-0">
+            <div class="mt-10 md:mt-60 mx-5 md:mx-20">
+              <div class="flex gap-3 items-center justify-start md:justify-center">
+                <img src="/public/asp.svg" alt="asp" width="30">
+                <h3 class="text-3xl font-bold">2022</h3>
+                <img src="/public/angular.svg" alt="angular" width="30">
+              </div>
+              <p class="mt-3 text-left md:text-center">
+                Попробовал веб по фуллстеку ASP.NET Core и Angular. Но затем полностью перешёл на фронтенд-разработку (Vue). Стажировался в BankaiShop, улучшал интерактивные карточки товаров, поиск и SEO сайта, добавил сохранение фильтров. 
+              </p>
+            </div>
+            <div class="mt-20 md:mt-40 mx-5 md:mx-20">
+              <div class="flex gap-2 items-center justify-start md:justify-center">
+                <img src="/public/WorldSkills.png" alt="" width="50">
+                <h3 class="text-3xl font-bold">2024</h3>
+              </div>
+              <p class="mt-3 text-left md:text-center">
+                Записался на WorldSkills по веб-разработке и получил свободное посещение. Выбрал стек Vue.js и Node.js. После закрытия BankaiShop ищу работу в крупных компаниях и создаю свой стартап.
+              </p>
+            </div>
+          </div>
         </div>
+      </div>
     </section>
-</template>
-
-
-
-
+  </template>
+  
